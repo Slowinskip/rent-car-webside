efd5665562b064b269fd0d5acd0f5e026fecccce
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-09 20:19:48 +0200

    Add navbar html structure

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import { Container, Navbar } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { FaCarAlt } from "react-icons/fa";
+const Header = () => {
+  return (
+    <Navbar>
+      <Container className="py-3 d-flex">
+        <Navbar.Brand className="d-flex">
+          <FaCarAlt className={styles.navLogo} />
+          <div className={styles.navTitle}>
+            <span>Rent</span>
+            <span>Car</span>
+          </div>
+        </Navbar.Brand>
+        <NavbarCollapse className={"justify-content-center " + styles.navLink}>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Vehicle</a>
+          <a href="">Contact</a>
+        </NavbarCollapse>
+        <div className={styles.navBnt}>
+          <button className="styles.loginBnt">Login</button>
+          <button className="styles.registerBnt">Register</button>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
