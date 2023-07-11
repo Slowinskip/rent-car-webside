aeb425f08299a2a26ffa9fd7507231c32b52ccad
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-11 22:02:17 +0200

    add button, image and css to models components

--- src/components/features/Models/Models.js
+++ src/components/features/Models/Models.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Models.module.scss";
-import { Col, Row } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Models = () => {
+  const cars = useSelector((store) => store.cars.car);
   return (
-    <div className={styles.modelContainer}>
-      <div className={"col-12 " + styles.modelsTitle}>
+    <Row className={styles.modelContainer}>
+      <div className={"col-7 " + styles.modelsTitle}>
         <h4>Vehicle Models</h4>
         <h3>Get to know our cars</h3>
         <p>
@@ -13,12 +15,62 @@ const Models = () => {
           the car before each rental.
         </p>
       </div>
-      <div className={styles.modelDetail}>
-        <Col className={styles.modelBtn}>1</Col>
-        <Col className={styles.modelImg}>2</Col>
-        <Col className={styles.modelInfo}>3</Col>
-      </div>
-    </div>
+      <Row className={" " + styles.modelDetail}>
+        <Col className={"col-12 col-md-6 col-lg-4 " + styles.modelBtn}>
+          {cars.map((car) => (
+            <Col>
+              <button>{car.model}</button>
+            </Col>
+          ))}
+        </Col>
+        <Col className={"col-12 col-md-6 col-lg-4 " + styles.modelImg}>
+          {" "}
+          <img
+            alt="img"
+            src="./image/mb-e-class-4d-blue-2021_preview_rev_1.png"
+          ></img>
+        </Col>
+        <Col className={"col-12 col-md-6 col-lg-4 " + styles.modelInfo}>
+          <Col className={"col-12 " + styles.modelPrice}>
+            <h5>
+              <span>$</span> / per day
+            </h5>
+          </Col>
+          <Col className={"col-12 " + styles.modelSpec}>
+            <div>
+              <h5>Model:</h5>
+            </div>
+            <div>
+              <h5>?</h5>
+            </div>
+          </Col>
+          <Col className={"col-12 " + styles.modelSpec}>
+            <div>
+              <h5>Mark:</h5>
+            </div>
+            <div>
+              <h5>?</h5>
+            </div>
+          </Col>
+          <Col className={"col-12 " + styles.modelSpec}>
+            <div>
+              <h5>Year:</h5>
+            </div>
+            <div>
+              <h5>?</h5>
+            </div>
+          </Col>
+          <Col className={"col-12 " + styles.modelSpec}>
+            <div>
+              <h5>Chest</h5>
+            </div>
+            <div>
+              <h5>?</h5>
+            </div>
+          </Col>
+        </Col>
+      </Row>
+    </Row>
   );
 };
 
