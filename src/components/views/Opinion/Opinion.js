c53fb86196d90ad8c840be3633e1520cba4b8d9c
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-13 22:24:51 +0200

    Add styles and media display to opinion components

--- src/components/views/Opinion/Opinion.js
+++ src/components/views/Opinion/Opinion.js
@@ -15,20 +15,35 @@ export const Opinion = () => {
         </p>
       </div>
       <Row className={styles.opinionBox} xs={1} md={2}>
-        <Col className={styles.opinionBox_col}>
-          <p className="col-12">p</p>
-          <div>
-            <img alt="img"></img>
-            <h6>1</h6>
-            <BsFillChatDotsFill />
+        <Col className={"" + styles.opinionBox_col}>
+          <p className="col-10">
+            Duis sit amet ex velit. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Aenean lacinia tellus ut diam tempus lacinia.
+            Cras elementum varius ligula a dignissim. Pellentesque iaculis
+            interdum sem sit amet lacinia. Pellentesque convallis dolor id
+            placerat tristique. Mauris aliquam erat sed metus mattis suscipit.
+            Aliquam eget ultrices nunc.
+          </p>
+          <div className={styles.opinionAuthorDiv}>
+            <img src="./image/person2.jpg" alt="img"></img>
+            <h4>Adam Smidth</h4>
+            <BsFillChatDotsFill className={styles.opinionIcon} />
           </div>
         </Col>
-        <Col className={styles.opinionBox_col}>
-          <p className="col-12">p</p>
-          <div>
-            <img alt="img"></img>
-            <h6>1</h6>
-            <BsFillChatDotsFill />
+        <Col className={"" + styles.opinionBox_col}>
+          <div className="col-10">
+            <p className="">
+              In a cursus leo, a varius enim. Nullam velit lorem, egestas in
+              faucibus sit amet, ullamcorper ut dolor. Sed tincidunt ex at ante
+              blandit vehicula. Nulla dui felis, mollis et quam vel, varius
+              tincidunt ante. Nunc vel mi eu leo convallis auctor eu nec leo.
+              Vestibulum congue sed nisl nec elementum.
+            </p>
+          </div>
+          <div className={styles.opinionAuthorDiv}>
+            <img src="./image/person1.jpg" alt="img"></img>
+            <h4>Samanta Dyson</h4>
+            <BsFillChatDotsFill className={styles.opinionIcon} />
           </div>
         </Col>
       </Row>
