b38a8e1d3a588540a4ec60384d58b3d99ff969e8
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-15 23:14:58 +0200

    Add styles to mobileApp components

--- src/components/views/MobileApp/MobileApp.js
+++ src/components/views/MobileApp/MobileApp.js
@@ -6,17 +6,17 @@ const MobileApp = () => {
   return (
     <div className={styles.container}>
       <Row xs={1} md={2}>
-        <Col className={styles.mobileText}>
-          <h4>Get to know our application</h4>
+        <Col className={"col-3 " + styles.mobileText}>
+          <h2>Get to know our application</h2>
           <p>
             In a ante dapibus, tempus urna eget, egestas eros. Quisque pulvinar
             vel odio quis eleifend. Ut consectetur tincidunt eros, laoreet
             faucibus eros facilisis eu. Donec luctus vestibulum aliquet. Donec
             in scelerisque augue, eget sodales mauris.
           </p>
-          <div className={styles.mobileText_icon}>
-            <BsApple />
-            <BsGooglePlay />
+          <div className={styles.mobileText_iconBox}>
+            <BsApple className={styles.icon} />
+            <BsGooglePlay className={styles.icon} />
           </div>
         </Col>
         <Col className={styles.mobileImage}>
