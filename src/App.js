e73dc4a057386760142c4c8d842111c36d41a85a
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-16 23:45:37 +0200

    Add routes to project, and two components: login, register

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import "./App.css";
 import Home from "./components/layout/Home/Home";
 import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/features/Login/Login";
+import Register from "./components/features/Register/Register";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
       <Footer />
     </>
   );
