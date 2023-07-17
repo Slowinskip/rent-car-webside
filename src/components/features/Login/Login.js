5388cd8f925fb0830d3719675d6a46ae7c41b097
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-17 23:52:06 +0200

    Add html code and styles to register component

--- src/components/features/Login/Login.js
+++ src/components/features/Login/Login.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.scss";
-import { Container, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Alert, Container, Form } from "react-bootstrap";
 
 const Login = () => {
+  const [error, setError] = useState(false);
+
   return (
     <Container className={styles.container}>
       <div className={"col-xs-8 col-md-4 " + styles.loginDiv}>
         <h4>LOGIN</h4>
+        {error === true && (
+          <Alert variant="danger" className="mt-3">
+            <Alert.Heading>Something is wrong!</Alert.Heading>
+            <p>
+              The date has not been selected or the pick-up date cannot be
+              before the start date
+            </p>
+          </Alert>
+        )}
+
         <Form.Label className={styles.loginForm}>Email</Form.Label>
         <input className={styles.loginInput} type="text"></input>
         <Form.Label className={styles.loginForm}>Password</Form.Label>
         <input className={styles.loginInput} type="password"></input>
         <div className={styles.loginBtnDiv}>
-          <a className={styles.loginBtn}>Login</a>
+          <a onClick={() => setError(!error)} className={styles.loginBtn}>
+            Login
+          </a>
           <a href="/register" className={styles.loginBtn}>
             Register
           </a>
--- src/components/features/Register/Register.js
+++ src/components/features/Register/Register.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Register.module.scss";
+import { Alert, Container, Form } from "react-bootstrap";
 
 const Register = () => {
-  return <div className={styles.container}>Register</div>;
+  const [error, setError] = useState(false);
+
+  return (
+    <Container className={styles.container}>
+      <div className={"col-xs-8 col-md-4 " + styles.registerDiv}>
+        <h4>REGISTER</h4>
+        {error ? (
+          <Alert variant="danger" className="mt-3">
+            <Alert.Heading>Something is wrong!</Alert.Heading>
+            <p>Sorry, this option doesn't work yet </p>
+          </Alert>
+        ) : (
+          ""
+        )}
+        <Form.Label className={styles.registerForm}>Email</Form.Label>
+        <input className={styles.registerInput} type="text"></input>
+        <Form.Label className={styles.registerForm}>Password</Form.Label>
+        <input className={styles.registerInput} type="password"></input>
+        <Form.Label className={styles.registerForm}>
+          Confirm password
+        </Form.Label>
+        <input className={styles.registerInput} type="password"></input>
+
+        <div className={styles.registerBtnDiv}>
+          <a onClick={() => setError(!error)} className={styles.registerBtn}>
+            Register
+          </a>
+          <a href="/login" className={styles.registerBtn}>
+            Login
+          </a>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Register;
